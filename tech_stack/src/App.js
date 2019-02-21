8536da38d1f192c16eefd770bda2626bb96419ce
Nelson Mendes <nelson.daniel.mendes@gmail.com>
2019-02-21 23:17:32 +0000

    library added

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { creatStore } from 'redux';
-import reducers from './reducers'
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
-export default class App extends Component {
-    render () {
-        return (
-            <Provider store={creatStore(reducers)}>
-                <View />
-            </Provider>
-        )
-    }
-}
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
 
-const styles = StyleSheet.create({
-    
-  });
+export default App;
